395afb6aa6ba04603366e62cbd35f2b03f3c792c
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-05-20 23:11:19 +0300

    Создано: класс, реализовывающий движение и поворот
    В классе реализованы методы обновления поворота и движения по направлению.

---
+++ Assets/Scripts/Core/Movement/NonInertialMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Core.Movement {
+	public class NonInertialMovement : Movement {
+		/// <summary>
+		/// Applies a rotation of eulerAngles.z degrees around the z-axis
+		/// </summary>
+		protected override void UpdateRotation() {
+			var currentAngleToRotate = AngleToRotate * (rotationSpeed * Time.deltaTime);
+			gameObject.transform.Rotate(currentAngleToRotate);
+		}
+
+		/// <summary>
+		/// Moves the transform in the direction and distance of translation.
+		/// </summary>
+		protected override void UpdateMovement() {
+			var offsetToMove = LocalDirectionToMove * (movementSpeed * Time.deltaTime);
+			gameObject.transform.Translate(offsetToMove);
+		}
+	}
+}
