b935f0385d5ded4501caa90f19f15ef087c96293
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-05-20 23:08:31 +0300

    Создан: абстрактный класс движения
    Добавляется к gameobject и отвечает за движение в сторону локального направления или поворот объекта

---
+++ Assets/Scripts/Core/Movement/Movement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Core.Movement {
+	public abstract class Movement : MonoBehaviour {
+		[Tooltip("Отзеркалить поворот объекта")]
+		[SerializeField] protected bool hasReversedRotation = true;
+
+		[SerializeField] protected float movementSpeed = 5;
+		[SerializeField] protected float rotationSpeed = 200;
+
+		private Vector3 _angleToRotate;
+
+		/// <summary>
+		/// Угол, на который будет поворачиваться объект при каждом вызове UpdateRotation
+		/// </summary>
+		public Vector3 AngleToRotate {
+			get => _angleToRotate;
+			set {
+				if (hasReversedRotation) {
+					_angleToRotate = -value;
+				}
+				else {
+					_angleToRotate = value;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Локальное направление, в сторону которого будет двигаться объект
+		/// </summary>
+		public Vector3 LocalDirectionToMove { get; set; }
+
+		protected abstract void UpdateRotation();
+		protected abstract void UpdateMovement();
+
+		private void Update() {
+			UpdateRotation();
+			UpdateMovement();
+		}
+	}
+}
