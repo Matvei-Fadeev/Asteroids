83ea0719e99023316eab8d0b4ad007e5ec32f782
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-05-21 17:53:06 +0300

    Смена папки и перенос комментариев
    Перенёс в подпапку Type. Коментарии перенесены к методам родителя.

--- Assets/Scripts/Core/Movement/Type/NonInertialMovement.cs
+++ Assets/Scripts/Core/Movement/Type/NonInertialMovement.cs
@@ -1,18 +1,12 @@
 ﻿using UnityEngine;
 
-namespace Core.Movement {
+namespace Core.Movement.Type {
 	public class NonInertialMovement : Movement {
-		/// <summary>
-		/// Applies a rotation of eulerAngles.z degrees around the z-axis
-		/// </summary>
 		protected override void UpdateRotation() {
 			var currentAngleToRotate = AngleToRotate * (rotationSpeed * Time.deltaTime);
 			gameObject.transform.Rotate(currentAngleToRotate);
 		}
 
-		/// <summary>
-		/// Moves the transform in the direction and distance of translation.
-		/// </summary>
 		protected override void UpdateMovement() {
 			var offsetToMove = LocalDirectionToMove * (movementSpeed * Time.deltaTime);
 			gameObject.transform.Translate(offsetToMove);
