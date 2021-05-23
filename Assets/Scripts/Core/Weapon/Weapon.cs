00f4101cf9b10dbe7340da694617248fb4c9a729
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-05-23 17:52:30 +0300

    Создан класс оружие, который отвечает за стрельбу пулями
    Берёт из очереди пули, и выстреливает в нужном направлении

---
+++ Assets/Scripts/Core/Weapon/Weapon.cs
@@ -0,0 +1,38 @@
+﻿using Input;
+using UnityEngine;
+
+namespace Core.Weapon {
+	public class Weapon : MonoBehaviour {
+		[SerializeField] private BulletPool bulletPool;
+		[SerializeField] private float shotDelay = 0.5f;
+		[SerializeField] private bool hasRandomDirection;
+
+		private IInputHandler _inputHandler;
+		private float _timeOfShot;
+
+		private void Awake() {
+			_inputHandler = GetComponent<IInputHandler>();
+		}
+
+		private void Update() {
+			if (_inputHandler.HasPressedShoot()) {
+				TakeShot();
+			}
+		}
+
+		private void TakeShot() {
+			if ((Time.time - _timeOfShot) > shotDelay) {
+				_timeOfShot = Time.time;
+				var bullet = bulletPool.GetBullet(transform.position);
+				bullet.Shoot(GetBulletDirection());
+			}
+		}
+
+		private Vector3 GetBulletDirection() {
+			if (hasRandomDirection) {
+				return UnityEngine.Random.insideUnitCircle.normalized;
+			}
+			return transform.up;
+		}
+	}
+}
