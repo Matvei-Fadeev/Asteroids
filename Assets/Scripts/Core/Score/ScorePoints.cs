534388b408f74b94455c47419842d08940bdd1aa
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-05-23 19:15:22 +0300

    Создан копмонент для объектов, повышающий Score
    Необходимо повесить на объект, в случае его уничтожения, будет увеличено количество очков

---
+++ Assets/Scripts/Core/Score/ScorePoints.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Core.Score {
+	/// <summary>
+	/// Необходимо повесить на объект, в случае его уничтожения, будет увеличено количество очков
+	/// </summary>
+	public class ScorePoints : MonoBehaviour {
+		[Tooltip("Количество очков, которое будет добавленно в общий счёт, при отключении объекта")]
+		[SerializeField] private int scoreAmount;
+
+		[Tooltip("Случа, при которых будут добавлены очки")]
+		[SerializeField] private WhenAddScore whenAddScore;
+
+		private enum WhenAddScore {
+			NONE,
+			ONDISABLE,
+			ONDESTROY,
+		}
+
+		private void OnDisable() {
+			if (whenAddScore == WhenAddScore.ONDISABLE) {
+				ScoreStorage.Add(scoreAmount);
+			}
+		}
+
+		private void OnDestroy() {
+			if (whenAddScore == WhenAddScore.ONDESTROY) {
+				ScoreStorage.Add(scoreAmount);
+			}
+		}
+	}
+}
