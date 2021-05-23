c26669dd7a306335017058dbe3103b70e953d525
Matvei-Fadeev <matwei.fadeew@gmail.com>
2021-05-23 19:14:24 +0300

    Создан дисплей очков
    Наподобие HealthDisplay, отображает текущее количество очков.

---
+++ Assets/Scripts/UI/ScoreDisplay/ScoreDisplay.cs
@@ -0,0 +1,29 @@
+﻿using Core.Score;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.ScoreDisplay {
+	public class ScoreDisplay : MonoBehaviour {
+		/// <summary>
+		/// Текст, который будет содержать в себе количество очков
+		/// </summary>
+		[SerializeField] private Text scoreText;
+
+		private void OnEnable() {
+			SetScoreText(ScoreStorage.CurrentScore);
+			ScoreStorage.ScoreUpdatedEvent += SetScoreText;
+		}
+
+		private void OnDisable() {
+			ScoreStorage.ScoreUpdatedEvent -= SetScoreText;
+		}
+
+		private void SetScoreText(int newScore) {
+			scoreText.text = newScore.ToString();
+		}
+
+		private void SetScoreText(object sender, int currentHealth) {
+			SetScoreText(currentHealth);
+		}
+	}
+}
